0cba338f30b26ec138f58e69f92e87a0872b31e9
Yuli <yuliatiyuli39@gmail.com>
2020-04-22 19:18:38 +0800

    Installed and Tested Firestore - Installed firebase-admin library for Firestore - Created firestoredb.py file to test database - Added a service account private key in JSON file for Firestore - Modified gitignore to exclude JSON file

---
+++ firestoredb.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('path/to/serviceAccount.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+#write
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
+
+#read
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
